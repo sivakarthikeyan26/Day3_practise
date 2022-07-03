253f2472dae73e1b24aa6acc83d19086f7a4a130
Your Name <you@example.com>
2022-07-03 14:22:46 +0530

    Program to print the elements of an array present on odd position

---
+++ src/day3/OddPositionElements.java
@@ -0,0 +1,25 @@
+package day3;
+
+import java.util.Scanner;
+
+public class OddPositionElements {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the size of array");
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		System.out.println("Enter the elements");
+		for (int i = 0; i < n; i++) {
+			int a = sc.nextInt();
+			arr[i] = a;
+		}
+		for (int i = 0; i < n; i++) {
+			if((i+1)%2!=0) {
+				System.out.print(arr[i]+" ");
+			}
+		}
+		sc.close();
+	}
+
+}
